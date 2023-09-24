668230b1d724835344e0666ce6d7f4a2effe9d89
pmyyyy <promp@LAPTOP-69L9RNF3>
2023-09-24 19:55:45 +0700

    update menu bar homepage design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Home = () => {
+  const linkStyle = {
+    color: 'gray',      // สีของข้อความ
+    textDecoration: 'none'  // ไม่มีเส้นใต้
+  };
+
   return (
     <nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
       <div className="container">
@@ -9,13 +14,13 @@ const Home = () => {
           <img src="src\images\IMG_5416.png" alt="Logo" width="30" height="24" className="d-inline-block align-text-top" />
            โรงเรียนฤทธิยะวรรณาลัย (ประถม)
          </Link>
-         <ul class="nav navbar-nav navbar-right">
-            <li><Link className='nav-link' to='/Register'>Register</Link></li>
-            <li><Link className='nav-link' to='/Login'>Log in</Link></li>
-        </ul>
+         <div className="nav navbar-nav navbar-right">
+            <span className='nav-link'>
+              <Link to='/Register' style={linkStyle}>Register</Link> | <Link to='/Login' style={linkStyle}>Log in</Link>
+            </span>
+        </div>
        </div>
     </nav> 
-    
   )
 }
 
